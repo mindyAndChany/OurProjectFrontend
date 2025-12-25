ae259cc4258363d05de14c29105c0975e914677f
mindi & chany <cp750905>
2025-12-25 19:27:03 +0200

    התחלתי להתעסק עם ניווטים

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
-import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Welcome from "./pages/Welcome.tsx";
-import HomePage from "./pages/HomePage";
-import Kattendence from "./pages/Kattendence";
-import Equipments from "./pages/Equipments";
-import Calendar from "./pages/Calendar";
-import './tailwind.css';
+import Routing from './Routing.jsx';
 
 function App() {
-  return (
-    <div className="min-h-screen bg-gray-50 text-gray-800 font-[Rubik]">
-      {/* <Navbar /> */}
-      <div className="p-8">
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/Equipments" element={<Equipments />} />
-          <Route path="/Calendar" element={<Calendar />} />
-          <Route path="/Kattendence" element={<Kattendence />} />
 
-        </Routes>
-      </div>
-    </div>
-  );
-}
 
+  return <Routing />;
+
+  }
 export default App;
---
+++ src/Routing.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+// ...existing code...
+import Welcome from './pages/Welcome.tsx';
+import HomePage from './pages/HomePage.jsx';
+import Login from './pages/Login.jsx';
+import Equipments from './pages/Equipments.jsx';
+import Calendar from './pages/Calendar.jsx';
+import Kattendence from './pages/Kattendence.jsx';
+
+export default function Routing() {
+//   return (
+//     <>
+//       <Navbar />
+//       <Routes>
+//       <div style={{padding:20}}>Routing בדיקה: נראה אותי?</div>
+
+//           <Route path="/" element={<Welcome />} />
+//           <Route path="/home" element={<HomePage />} />
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/Equipments" element={<Equipments />} />
+//           <Route path="/Calendar" element={<Calendar />} />
+//           <Route path="/Kattendence" element={<Kattendence />} />
+
+//       </Routes>
+//   </>
+//   );
+  return (
+ <>
+      <Navbar />
+          <Routes>
+
+           <Route path="/" element={<Welcome />} />
+           <Route path="/home" element={<HomePage />} />
+           <Route path="/login" element={<Login />} />     
+           <Route path="/Equipments" element={<Equipments />} />
+           <Route path="/Calendar" element={<Calendar />} />
+           <Route path="/Kattendence" element={<Kattendence />} />
+
+       </Routes>
+      <div style={{padding:20}}>Routing בדיקה: נראה אותי?</div>
+</>
+  );
+
+}
