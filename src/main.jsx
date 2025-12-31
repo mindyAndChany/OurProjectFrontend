0de871f7b52dfd169f1e53cd0491c85408792337
mindi32792 <   mindi32792>
2025-12-31 23:00:26 +0200

    הוספת לוח שנה

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";       // ← חובה
-import { STORE } from './redux/store.js';     // ← אתה כבר מייבא נכון
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 import "./tailwind.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={STORE}>                    {/* ← עוטף את האפליקציה */}
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
