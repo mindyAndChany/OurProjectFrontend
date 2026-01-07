911750c41599f1dd63f78fbd131f5d8dade4bfe5
chana <r8060402@gmail.com>
2026-01-07 14:42:53 +0200

    קידוד נכון לURL

--- src/redux/slices/STUDENTS/getStudentDataThunk.js
+++ src/redux/slices/STUDENTS/getStudentDataThunk.js
@@ -1,17 +1,17 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const getStudentDataThunk = createAsyncThunk(
-    'getstudentData',
-    async (catgories) => {
-        const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/studentsData/getstudentData/${catgories}`);
+  'getstudentData',
+  async (categories) => {
+    const encodedCategories = encodeURIComponent(categories);
+    const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/studentsData/getstudentData/${encodedCategories}`);
 
-        if (res.ok) {
-            const data = await res.json();
-            console.log("fetch studentData success get ");
-            return data;
-        } 
-        else {
-            throw new Error('failed to fetch');
-        }
+    if (res.ok) {
+      const data = await res.json();
+      console.log("fetch studentData success get ");
+      return data;
+    } else {
+      throw new Error('failed to fetch');
     }
+  }
 );
