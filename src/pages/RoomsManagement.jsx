84fa5aa5b12f69e5e4ac0173ac7a52a449107d44
chana <r8060402@gmail.com>
2026-03-02 22:22:07 +0200

    בדיקת זמינות בזמן אמת

--- src/pages/RoomsManagement.jsx
+++ src/pages/RoomsManagement.jsx
@@ -86,7 +86,7 @@ const RoomsManagement = ({
                       onClick={handleResetAvailability}
                       className="bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-700 transition-colors"
                     >
-                      ╳ ריסט
+                      ╳ ביטול 
                     </button>
                   )}
                 </div>
